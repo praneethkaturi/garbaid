f28a93fdac61ec532ca9f0c274c5b6190124488c
Praneeth Katuri <praneeth.katuri007@gmail.com>
2020-10-02 16:56:02 +0530

    requirements.txt, dockerfile for flask

--- server/flask-app/app.py
+++ server/flask-app/app.py
@@ -1,7 +1,19 @@
-from flask import Flask,request
-from tensorflow.keras.models import load_model
+import io
+import base64
+from flask import Flask,request,jsonify
+from PIL import Image
+##from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
 
 app = Flask(__name__)
+
+def stringToImage(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    return Image.open(io.BytesIO(imgdata))
+
+def toRGB(image):
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)    
 
 @app.route('/')
 def home():
@@ -9,12 +21,23 @@ def home():
 
 @app.route('/predict', methods = ['POST'])
 def predict_class():
+    IMAGE_RESIZE = 224
     req_data = request.get_json()
+
+    ## Loading model
+    ##model = load_model('../../model/utils/weights/untrained.h5')
+
+    ## Converting base64 image to cv2 readable image
     image_base64 = req_data['image']
-    latitude = req_data['location'][0]
-    longitude = req_data['location'][1]
-    print('Image in base64: {}\nLatitude: {}, Longitude: {}'.format(image_base64, latitude, longitude))
-    return req_data
+    PIL_image = stringToImage(image_base64)
+    image = toRGB(PIL_image)
+
+    ## Predicting class 
+    image_resized = cv2.resize(image, (IMAGE_RESIZE,IMAGE_RESIZE))
+    ##pred = model.predict(image_resized) 
+    ##class_value = np.argmax(pred, axis = 0)
+
+    return jsonify({"class": 0})
 
 if __name__ == '__main__':
     app.run()    
